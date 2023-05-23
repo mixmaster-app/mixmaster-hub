2846cd5c53163b43cb44094b99d929d7a58f3802
Kiio__ <caillaud.hugo.44@gmail.com>
2023-05-23 23:37:30 +0200

    Fix fields and modal

--- src/views/Settings/Components/SettingsList/DevelopperMode/ServerList.vue
+++ src/views/Settings/Components/SettingsList/DevelopperMode/ServerList.vue
@@ -5,10 +5,11 @@
         <v-toolbar-title>Serveurs</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
+
+        <v-dialog v-model="isEditModalOpen" max-width="600px">
           <template v-slot:activator="{ on, attrs }">
             <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Item
+              Ajouter un serveur
             </v-btn>
           </template>
           <v-card>
@@ -20,13 +21,24 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Serveur" />
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Nom du Serveur"
+                    />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.ip_address"
+                      label="Addresse Ip"
+                    />
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.ip_address" label="Ip" />
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.port" label="Port" />
+                    <v-text-field
+                      v-model="editedItem.port"
+                      label="Port du Serveur"
+                    />
                   </v-col>
                 </v-row>
               </v-container>
@@ -43,18 +55,19 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
+
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
-            <v-card-title class="text-h5">
-              Are you sure you want to delete this item?
+            <v-card-title class="text-h6">
+              Voulez vous vraiment supprimer ce serveur ?
             </v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue darken-1" text @click="closeDelete">
-                Cancel
+                Non
               </v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">
-                OK
+              <v-btn color="red darken-1" text @click="deleteItemConfirm">
+                Oui
               </v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
@@ -63,17 +76,17 @@
       </v-toolbar>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">
-        mdi-pencil
-      </v-icon>
-      <v-icon small @click="deleteItem(item)">
-        mdi-delete
-      </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">
-        Reset
-      </v-btn>
+      <div v-if="item.isEditable">
+        <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon>
+      </div>
+      <div v-else>
+        <span class="text--disabled">Non éditable</span>
+      </div>
     </template>
   </v-data-table>
 </template>
@@ -82,30 +95,32 @@
 export default {
   name: "ServerList",
   data: () => ({
-    dialog: false,
+    isEditModalOpen: false,
     dialogDelete: false,
     headers: [
       {
         text: "Nom du serveur",
-        align: "start",
+        align: "center",
         sortable: false,
         value: "name"
       },
-      { text: "Addresse IP", value: "ip_address" },
-      { text: "Port", value: "port" },
-      { text: "Actions", value: "actions", sortable: false }
+      { text: "Addresse IP", align: "center", value: "ip_address" },
+      { text: "Port", align: "center", value: "port" },
+      { text: "Actions", align: "center", value: "actions", sortable: false }
     ],
     servers: [],
     editedIndex: -1,
     editedItem: {
       name: "",
       ip_address: "",
-      port: ""
+      port: "",
+      isEditable: true
     },
     defaultItem: {
       name: "",
       ip_address: "",
-      port: ""
+      port: "",
+      isEditable: true
     }
   }),
 
@@ -136,14 +151,15 @@ export default {
         {
           name: "Mixmaster FR",
           ip_address: "1.2.3.4",
-          port: "8080"
+          port: "8080",
+          isEditable: false
         }
       ];
     },
     editItem(item) {
       this.editedIndex = this.servers.indexOf(item);
       this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+      this.isEditModalOpen = true;
     },
 
     deleteItem(item) {
@@ -158,7 +174,7 @@ export default {
     },
 
     close() {
-      this.dialog = false
+      this.isEditModalOpen = false;
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
@@ -166,7 +182,7 @@ export default {
     },
 
     closeDelete() {
-      this.dialogDelete = false
+      this.dialogDelete = false;
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
